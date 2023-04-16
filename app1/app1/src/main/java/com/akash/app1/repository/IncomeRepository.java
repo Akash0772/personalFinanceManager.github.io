4498d0dbb0ddb1bc0c3a06420573e3ffc9cdc5c9
Akash Chaurasiya <akashchaurasia617@gmail.com>
2023-04-17 04:24:21 +0530

    Income Repository Class

--- app1/app1/src/main/java/com/akash/app1/repository/IncomeRepository.java
+++ app1/app1/src/main/java/com/akash/app1/repository/IncomeRepository.java
@@ -1,5 +1,8 @@
 package com.akash.app1.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,7 @@ import com.akash.app1.entity.Income;
 
 @Repository
 public interface IncomeRepository extends JpaRepository<Income, Long> {
+
+	List<Income> findByDateBetween(LocalDate startDate, LocalDate endDate);
 
 }
