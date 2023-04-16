404034e98865f636e71ba43a41090710db7e3053
Akash Chaurasiya <akashchaurasia617@gmail.com>
2023-04-17 04:24:10 +0530

    Expense Repository Class

--- app1/app1/src/main/java/com/akash/app1/repository/ExpenseRepository.java
+++ app1/app1/src/main/java/com/akash/app1/repository/ExpenseRepository.java
@@ -1,5 +1,8 @@
 package com.akash.app1.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,7 @@ import com.akash.app1.entity.Expense;
 
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long>{
+
+	List<Expense> findByDateBetween(LocalDate startDate, LocalDate endDate);
 	
 }
